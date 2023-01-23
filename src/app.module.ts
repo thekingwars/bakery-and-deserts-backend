767ac32fecb70b2b8c6f4ec4aaa1493bf9cb2a5e
carlosEleva <carlos@eleva.school.com>
2023-01-22 20:49:07 -0400

    ready todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { CartModule } from './cart/cart.module';
 import { ProductsModule } from './products/products.module';
+import { UserModule } from './users/user.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { ProductsModule } from './products/products.module';
     databaseModule,
     CartModule,
     ProductsModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],

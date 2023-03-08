5a246a3f2bdb4dcab531f5af4c2c8be7e5fc721c
pablo castillo villalobos <hgmopikr@hotmail.com>
2023-03-08 14:49:11 -0400

    update_carrito

--- src/cart/controller/cart.controller.ts
+++ src/cart/controller/cart.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { JwtGuard } from 'src/users/guards/jwt.guard';
+import { CartDto } from './../dto/cart';
+import { CartService } from 'src/cart/services/cart.service';
+import { Body, Controller, Get, Post, Put, UseGuards } from '@nestjs/common';
 
-@Controller('cart')
-export class CartController {}
+@Controller('/api/cart')
+export class CartController {
+  constructor(private cartService: CartService) {}
+
+  @Get()
+  @UseGuards(JwtGuard)
+  findCart(@Body() { _id, products }: CartDto) {
+    return this.cartService.findCart(_id, products);
+  }
+
+  @Post('create')
+  @UseGuards(JwtGuard)
+  async createCartByRegisterUser(@Body() payload: CartDto) {
+    return this.cartService.createCartByRegisterUser({ ...payload });
+  }
+
+  @UseGuards(JwtGuard)
+  @Put('update')
+  async updateCartProduct(@Body() payload: CartDto) {
+    return this.cartService.updateCart({
+      ...payload,
+    });
+  }
+
+  @UseGuards(JwtGuard)
+  @Put('empty')
+  async emptyCart(@Body() payload: CartDto) {
+    return this.cartService.emptyCart({
+      ...payload,
+    });
+  }
+}
--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Product } from 'src/products/models/product';
 import { CartDto } from '../dto/cart';
 import { CartEntity } from '../entities/cart';
 
@@ -17,4 +18,91 @@ export class CartService {
 
     return CartDto.cartJSON(saveCart.toJSON);
   }
+
+  async updateCart(cartDto: CartDto) {
+    try {
+      const findCart = await this.cartEntity.findOne({ _id: cartDto._id });
+
+      const products = [...findCart.products, ...cartDto.products];
+
+      const cart = await this.cartEntity.findOneAndUpdate(
+        { _id: cartDto._id },
+        { products },
+      );
+      return cart;
+    } catch (err) {
+      throw new HttpException(
+        'Ha ocurrido un error, intentalo mas tarde',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async emptyCart(cartDto: CartDto) {
+    try {
+      const cart = await this.cartEntity.findOneAndUpdate(
+        { _id: cartDto._id },
+        { products: [] },
+      );
+      return cart;
+    } catch (err) {
+      throw new HttpException(
+        'Ha ocurrido un error, intentalo mas tarde',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findCart(id: string, product: Product[]) {
+    const cart = await this.cartEntity
+      .find({ _id: id, products: product })
+      .populate('products.product');
+
+    /*if (!cart) {
+      throw new HttpException(
+        'No ha añadido ningun producto',
+        HttpStatus.NO_CONTENT,
+      );
+    }*/
+    return cart;
+  }
+
+  /*async findOneProduct(id: string){
+    return this.cartEntity.findOne({_id})
+  }
+
+  async findCartProducts(products: Array<Product>) {
+    let product;
+    try {
+      product = await this.cartEntity.findById(products);
+    } catch (err) {
+      throw new HttpException(
+        'Ha ocurrido un error, intentalo mas tarde',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    if (!product) {
+      throw new NotFoundException('No se encontraron los productos.');
+    }
+    return product;
+  }
+
+  async updateCartProduct(cartDto: CartDto) {
+    const updatedCartProduct = await this.findOne();
+
+    updatedCartProduct.save();
+    try {
+      await this.cartEntity.updateOne(
+        { products: cartDto.products },
+        {
+          ...cartDto,
+        },
+      );
+    } catch (err) {
+      throw new HttpException(
+        'Ha ocurrido un error, intentalo mas tarde',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }*/
 }
